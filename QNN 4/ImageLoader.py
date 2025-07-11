3b7926bfb10001da6adb33a830c71fcb426aa772
Rui Rebich Hespanha <rui.hespanha@gmail.com>
2025-07-11 12:11:00 -0700

    edited folders and fixed Image loader
    Co-authored-by: sidved890 <sidved890@users.noreply.github.com>

--- QNN 4/ImageLoader.py
+++ QNN 4/ImageLoader.py
@@ -13,39 +13,47 @@ def ImageLoader(folder, num):
     labels = []
     label_num = 0
     for sub_folder in os.listdir(folder):
-        for sub_sub_folder in os.listdir(folder):
-            count = 0
-            for filename in os.listdir(
-                f"{folder}" + "\\" + f"{sub_folder}" + "\\" + f"{sub_sub_folder}"
-            ):
-                count += 1
-                if (count >= num + 1) and (num != -1):
-                    print("hit")
-                    break
+        if sub_folder != ".DS_Store":
+            for sub_sub_folder in os.listdir(f"{folder}" + "\\" + f"{sub_folder}"):
+                if sub_sub_folder != ".DS_Store":
+                    count = 0
+                    for filename in os.listdir(
+                        f"{folder}"
+                        + "\\"
+                        + f"{sub_folder}"
+                        + "\\"
+                        + f"{sub_sub_folder}"
+                    ):
+                        if filename != ".DS_Store":
+                            count += 1
+                            if (count >= num + 1) and (num != -1):
+                                break
 
-                print(
-                    f"{sub_folder} : {sub_sub_folder}: {filename}, {count}/{num}",
-                    end="\r",
-                    flush=True,
-                )
-                try:
-                    img = (
-                        Image.open(
-                            f"{folder}"
-                            + "\\"
-                            + f"{sub_folder}"
-                            + "\\"
-                            + f"{sub_sub_folder}"
-                            + "\\"
-                            + f"{filename}"
-                        )
-                        .convert("L")
-                        .resize((IMG_SIZE, IMG_SIZE))
-                    )
-                    images.append(np.array(img) / 255.0)
-                    labels.append(label_num)
-                except:
-                    print(f"Error loading image: {folder} , {filename}")
-            label_num += 1
+                            print(
+                                f"{sub_folder} : {sub_sub_folder}: {filename}, {count}/{num}",
+                                end="\r",
+                                flush=True,
+                            )
+                            try:
+                                img = (
+                                    Image.open(
+                                        f"{folder}"
+                                        + "\\"
+                                        + f"{sub_folder}"
+                                        + "\\"
+                                        + f"{sub_sub_folder}"
+                                        + "\\"
+                                        + f"{filename}"
+                                    )
+                                    .convert("L")
+                                    .resize((IMG_SIZE, IMG_SIZE))
+                                )
+                                images.append(np.array(img) / 255.0)
+                                labels.append(label_num)
+                                view.append(int(sub_sub_folder))
+                            except:
+                                print(f"Error loading image: {folder} , {filename}")
+                    label_num += 1
+                print("")
 
     return np.stack(images), np.array(labels)
